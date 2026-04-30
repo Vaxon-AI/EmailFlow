1b40d50d320a0396f960308cec1f3aa09799f98d
Evian-Zh <evianmeow@gmail.com>
2026-04-30 22:55:52 +1000

    Remove unused password card code

--- src/components/email-review-modal.tsx
+++ src/components/email-review-modal.tsx
@@ -56,7 +56,11 @@ export function EmailReviewModal({ open, onClose }: Props) {
   const toggleSelect = (id: string) => {
     setSelected((prev) => {
       const next = new Set(prev)
-      next.has(id) ? next.delete(id) : next.add(id)
+      if (next.has(id)) {
+        next.delete(id)
+      } else {
+        next.add(id)
+      }
       return next
     })
   }
